a409ec5d668f615362604008835205ed881032bf
KennyTV <kennytv@t-online.de>
2020-04-10 23:42:06 +0200

    Update copyright headers

--- maintenance-core/src/main/java/eu/kennytv/maintenance/core/dump/MaintenanceDump.java
+++ maintenance-core/src/main/java/eu/kennytv/maintenance/core/dump/MaintenanceDump.java
@@ -1,6 +1,6 @@
 /*
  * Maintenance - https://git.io/maintenancemode
- * Copyright (C) 2018 KennyTV (https://github.com/KennyTV)
+ * Copyright (C) 2018-2020 KennyTV (https://github.com/KennyTV)
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
